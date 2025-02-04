30e992654902f6746d893789e431258e4360eefd
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-04 20:22:09 +0300

    remove dictionaries from right when change menu item

--- static/viki_web_cms/js/main.js
+++ static/viki_web_cms/js/main.js
@@ -5,16 +5,19 @@ export const jsonUrl = './json/'
 
 const menuItems = document.querySelectorAll('.menu__item');
 const contentLeft = document.querySelector('.content__left');
+const contentRight = document.querySelector('.content__right');
 
 menuItems.forEach(menuItem => {
     menuItem.addEventListener('click', (event) => {
         if (!menuItem.classList.contains('menu__item_bold')) {
             menuItem.classList.add('menu__item_bold')
             contentLeft.innerHTML = '';
+            contentRight.innerHTML = '';
             createContentLeft(contentLeft, menuItem.dataset.content)
         }
         menuItems.forEach(item => {
             item !== menuItem ? item.classList.remove('menu__item_bold') : null;
+
         })
     })
 });
