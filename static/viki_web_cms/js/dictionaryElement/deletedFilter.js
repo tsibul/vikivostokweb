4adf3cc2824e9a1f15029e5fad9639e6b3ae8b5f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-05 23:28:05 +0300

    moved code to reloadContent

--- static/viki_web_cms/js/dictionaryElement/deletedFilter.js
+++ static/viki_web_cms/js/dictionaryElement/deletedFilter.js
@@ -1,16 +1,9 @@
 'use strict';
 
-import {createDictionaryContent} from "./showDictionary/createDictionaryContent.js";
+import {reloadContent} from "./showDictionary/reloadContent.js";
 
 export async function deletedFilter(className, deletedCheck) {
-    const searchString = deletedCheck.closest('.dictionary-frame__header_right')
-        .querySelector('.dictionary-frame__input')
-        .textContent;
     const dictionarySection = deletedCheck.closest('.dictionary-frame');
-    let dictionaryContent = dictionarySection.querySelector('.dictionary-content');
-    dictionaryContent.remove();
-    dictionaryContent = await createDictionaryContent(className,
-        deletedCheck.checked ? 0 : 1,
-        searchString ? searchString : 'None');
-    dictionarySection.appendChild(dictionaryContent);
+    const searchString = dictionarySection.querySelector('.dictionary-frame__input').textContent;
+    await reloadContent(dictionarySection,className,deletedCheck, searchString);
 }
