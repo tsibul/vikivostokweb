54367724a74a570fd2b2bacd5972f090582c3544
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-05 23:43:06 +0300

    ready

--- static/viki_web_cms/js/dictionaryElement/searchFilter.js
+++ static/viki_web_cms/js/dictionaryElement/searchFilter.js
@@ -1,9 +1,10 @@
 'use strict'
 
+import {reloadContent} from "./reloadContent.js";
+
 export async function searchFilter(button, className) {
     const dictionarySection = button.closest('.dictionary-frame');
     const searchString = dictionarySection.querySelector('.dictionary-frame__input').textContent;
     const deletedCheck = dictionarySection.querySelector('.check');
-
-
+    await reloadContent(dictionarySection, className, deletedCheck, searchString);
 }
