6a33315b1f853801605552656dae61690389d047
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-05 23:43:24 +0300

    initial commit

---
+++ static/viki_web_cms/js/dictionaryElement/showDictionary/clearSearchFilter.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import {reloadContent} from "../reloadContent.js";
+
+export async function clearSearchFilter(button, className) {
+    const dictionarySection = button.closest('.dictionary-frame');
+    dictionarySection.querySelector('.dictionary-frame__input').textContent = '';
+    const deletedCheck = dictionarySection.querySelector('.check');
+    await reloadContent(dictionarySection, className, deletedCheck, '');
+}
