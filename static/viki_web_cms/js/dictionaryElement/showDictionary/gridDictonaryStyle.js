fe8440f47e685b8537f2e69befe1723df55b895f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-05 22:27:42 +0300

    first column for name longer (6fr)

--- static/viki_web_cms/js/dictionaryElement/showDictionary/gridDictonaryStyle.js
+++ static/viki_web_cms/js/dictionaryElement/showDictionary/gridDictonaryStyle.js
@@ -1,12 +1,14 @@
 'use strict'
 
 export function gridDictionaryStyle(fields) {
-    let elementStile = '14px ';
+    let elementStile = '14px 6fr ';
     fields.forEach(field => {
-        field.type === 'boolean' || field.type === 'number'
-            ? elementStile = elementStile + '1fr '
-            : elementStile = elementStile + '4fr ';
-    })
+        if (field.field !== 'name') {
+            field.type === 'boolean' || field.type === 'number'
+                ? elementStile = elementStile + '1fr '
+                : elementStile = elementStile + '3fr ';
+        }
+    });
     elementStile = elementStile + '80px'
     return elementStile;
 }
