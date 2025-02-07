2e97036c77477c8f8a4bab6e1b3f726ab8a02c04
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-07 09:01:58 +0300

    comment added

--- static/viki_web_cms/js/dictionaryElement/reloadContent.js
+++ static/viki_web_cms/js/dictionaryElement/reloadContent.js
@@ -3,6 +3,15 @@
 
 import {createDictionaryContent} from "./showDictionary/createDictionaryContent.js";
 
+/**
+ * reload content for dictionarySection when query params changed
+ * @param dictionarySection section to reload content
+ * Queri parameters
+ * @param className className of the dictionary
+ * @param deletedCheck if deleted checked true
+ * @param searchString value of searchString input
+ * @returns {Promise<void>}
+ */
 export async function reloadContent(dictionarySection, className, deletedCheck, searchString) {
     let dictionaryContent = dictionarySection.querySelector('.dictionary-content');
     dictionaryContent.remove();
