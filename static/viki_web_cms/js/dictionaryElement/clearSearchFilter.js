3a554c3e0d8aa9281359d3641a29b5cd1b552ee7
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-07 13:27:12 +0300

    comment added

--- static/viki_web_cms/js/dictionaryElement/clearSearchFilter.js
+++ static/viki_web_cms/js/dictionaryElement/clearSearchFilter.js
@@ -3,9 +3,15 @@
 
 import {reloadContent} from "./reloadContent.js";
 
+/**
+ * clear searchString on click and remove search results
+ * @param button
+ * @param className
+ * @returns {Promise<void>}
+ */
 export async function clearSearchFilter(button, className) {
     const dictionarySection = button.closest('.dictionary-frame');
     dictionarySection.querySelector('.dictionary-frame__input').value = '';
     const deletedCheck = dictionarySection.querySelector('.check');
-    await reloadContent(dictionarySection, className, deletedCheck, '');
+    await reloadContent(dictionarySection, className, deletedCheck, 'None');
 }
