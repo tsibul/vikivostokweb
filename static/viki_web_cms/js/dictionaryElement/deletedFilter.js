b358d366c0ed3b2da3547a8150c84a890b22d1f2
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-07 11:23:45 +0300

    search string validator and comment added

--- static/viki_web_cms/js/dictionaryElement/deletedFilter.js
+++ static/viki_web_cms/js/dictionaryElement/deletedFilter.js
@@ -2,9 +2,17 @@
 
 
 import {reloadContent} from "./reloadContent.js";
+import {searchStringValidator} from "./searchStringValidator.js";
 
+/**
+ * reload content onchange deletedCheck
+ * @param className className for selected dictionary
+ * @param deletedCheck checked element
+ * @returns {Promise<void>}
+ */
 export async function deletedFilter(className, deletedCheck) {
     const dictionarySection = deletedCheck.closest('.dictionary-frame');
-    const searchString = dictionarySection.querySelector('.dictionary-frame__input').textContent;
+    let searchString = dictionarySection.querySelector('.dictionary-frame__input').textContent;
+    !searchStringValidator(searchString) ? searchString = 'None' : null;
     await reloadContent(dictionarySection,className,deletedCheck, searchString);
 }
