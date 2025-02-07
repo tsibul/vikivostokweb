b30ee8c0d8b6065eae675b24eae1923949b199e2
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-07 11:42:33 +0300

    added searchStringNormalizer and comment

--- static/viki_web_cms/js/dictionaryElement/searchFilter.js
+++ static/viki_web_cms/js/dictionaryElement/searchFilter.js
@@ -1,10 +1,17 @@
 'use strict'
 
 import {reloadContent} from "./reloadContent.js";
+import {searchStringNormalizer} from "./searchStringNormalizer.js";
 
+/**
+ * reload dictionary content onclick search button
+ * @param button search button
+ * @param className
+ * @returns {Promise<void>}
+ */
 export async function searchFilter(button, className) {
     const dictionarySection = button.closest('.dictionary-frame');
     const searchString = dictionarySection.querySelector('.dictionary-frame__input').value;
     const deletedCheck = dictionarySection.querySelector('.check');
-    await reloadContent(dictionarySection, className, deletedCheck, searchString);
+    await reloadContent(dictionarySection, className, deletedCheck, searchStringNormalizer(searchString));
 }
