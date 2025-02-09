9709c1a097e02c7c6c70636e01ade88ed004949f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-09 22:20:59 +0300

    added GoodsGroup added menu for printing and filter without classes removed clients managers & order as it should be in separate application

--- viki_web_cms/classes/main_menu.py
+++ viki_web_cms/classes/main_menu.py
@@ -2,7 +2,7 @@ import json
 
 
 class CMSSetting:
-    def __init__(self, setting: str, setting_class):
+    def __init__(self, setting: str, setting_class: str):
         self.setting = setting
         self.setting_class = setting_class
 
@@ -25,45 +25,71 @@ class MainMenu:
     @staticmethod
     def menu_set():
         menu_list = [
+            MainMenu('Номенклатура', str(json.dumps([
+                MenuSection('Номенклатура', [
+                    CMSSetting('Товары/артикулы', 'Goods'),
+                ]),
+            ], default=lambda o: o.__dict__,
+                sort_keys=True))),
+            MainMenu('Каталог', str(json.dumps([
+                MenuSection('Каталог', [
+                    CMSSetting('Каталог', ''),
+                ]),
+            ], default=lambda o: o.__dict__,
+                sort_keys=True))),
+            MainMenu('Прайс-лист', str(json.dumps([
+                MenuSection('Прайс-лист', [
+                    CMSSetting('Прайс-лист', ''),
+                ]),
+            ], default=lambda o: o.__dict__,
+                sort_keys=True))),
             MainMenu("Настройки", str(json.dumps([
 
                 MenuSection('Цвета, материалы', [
                     CMSSetting('Материал', 'MaterialType'),
                     CMSSetting('Цветовая Схема', 'ColorScheme'),
-                    CMSSetting('Цветовая Группа', 'ColorGroup'),
-                    CMSSetting('Цвет', 'Color'),
+                    CMSSetting('Цветовая Группа', 'ColorGroup' ),
+                    CMSSetting('Цвет', 'Color' ),
+                ]),
+                MenuSection('Настройки товаров', [
+                    CMSSetting('Группы каталога', 'ProductGroup' ),
+                    CMSSetting('Группы товара', 'GoodsGroup' ),
+                    CMSSetting('Опции товара', 'GoodsOption'),
+                    CMSSetting('Связь товаров и опций', 'GoodsToOption'),
+                ]),
+                MenuSection('Фото товаров', [
+                    CMSSetting('Фото товаров', ''),
                 ]),
                 MenuSection('Отображение', [
                     CMSSetting('Интерфейс товара', 'LayoutType'),
+                ]),
+                MenuSection('Нанесение', [
+                    CMSSetting('Тип нанесения', ''),
+                    CMSSetting('Место нанесения', ''),
+                ]),
+                MenuSection('Дополнительные фильтры', [
+                    CMSSetting('Фильтры', ''),
+                    CMSSetting('Фильтры', ''),
                 ]),
                 MenuSection('PANTONE-HEX', [
                     CMSSetting('PANTONE-HEX', 'PantoneToHex'),
                 ]),
             ], default=lambda o: o.__dict__,
                 sort_keys=True))),
-            MainMenu('Номенклатура', str(json.dumps([
-                MenuSection('Группы', [
-                    CMSSetting('Группы товара', 'ProductGroup'),
-                    CMSSetting('Товары/артикулы', 'Goods'),
-                    CMSSetting('Опции товара', 'GoodsOption'),
-                    CMSSetting('Связь товаров и опций', 'GoodsToOption'),
-                ]),
-            ], default=lambda o: o.__dict__,
-                sort_keys=True))),
-            MainMenu('Менеджеры', str(json.dumps([
-                MenuSection('Менеджеры', [
-                ]),
-            ], default=lambda o: o.__dict__,
-                sort_keys=True))),
-            MainMenu('Клиенты', str(json.dumps([
-                MenuSection('Клиенты', [
-                ]),
-            ], default=lambda o: o.__dict__,
-                sort_keys=True))),
-            MainMenu('Заказы', str(json.dumps([
-                MenuSection('Заказы', [
-                ]),
-            ], default=lambda o: o.__dict__,
-                sort_keys=True))),
+        #     MainMenu('Менеджеры', str(json.dumps([
+        #         MenuSection('Менеджеры', [
+        #         ]),
+        #     ], default=lambda o: o.__dict__,
+        #         sort_keys=True))),
+        #     MainMenu('Клиенты', str(json.dumps([
+        #         MenuSection('Клиенты', [
+        #         ]),
+        #     ], default=lambda o: o.__dict__,
+        #         sort_keys=True))),
+        #     MainMenu('Заказы', str(json.dumps([
+        #         MenuSection('Заказы', [
+        #         ]),
+        #     ], default=lambda o: o.__dict__,
+        #         sort_keys=True))),
         ]
         return menu_list
