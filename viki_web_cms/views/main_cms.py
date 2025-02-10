8738aeab880f7ffe2a42947a3c225caa52d58cc7
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-11 00:30:03 +0300

    remove unused

--- viki_web_cms/views/main_cms.py
+++ viki_web_cms/views/main_cms.py
@@ -4,6 +4,5 @@ from viki_web_cms.classes.main_menu import MainMenu
 
 def main_cms(request):
     main_menu = MainMenu.menu_set()
-    menu_out = []
     context = {'menu': main_menu, 'user': request.user.username}
     return render(request, 'cms.html', context)
