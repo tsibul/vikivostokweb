8a720c3aecb24453539de76a52efb65b9afb8846
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-11 12:35:35 +0300

    Create model FilterOption + Create model FilterToGoods + Create model FilterToGoodsGroup

--- viki_web_cms/models/goods_filter.py
+++ viki_web_cms/models/goods_filter.py
@@ -1,25 +1,75 @@
 from django.db import models
 
-from viki_web_cms.models import SettingsDictionary
+from viki_web_cms.models import SettingsDictionary, Goods, GoodsGroup
 
 
-class GoodsFilter(SettingsDictionary):
-    """ nearest color """
-    multicolor = models.BooleanField(default=True)
+class FilterOption(SettingsDictionary):
+    """ filter names """
 
     class Meta(SettingsDictionary.Meta):
-        verbose_name = 'Цветовая группа'
-        verbose_name_plural = 'Цветовые группы'
-        db_table_comment = 'nearest color'
-        db_table = 'color_group'
+        verbose_name = 'Фильтр'
+        verbose_name_plural = 'Фильтры'
+        db_table_comment = 'Filter Options'
+        db_table = 'filter_options'
+
+
+class FilterToGoods(SettingsDictionary):
+    """ goods filter set """
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    filter_option = models.ForeignKey(FilterOption, on_delete=models.CASCADE)
+
+    class Meta(SettingsDictionary.Meta):
+        verbose_name = 'Фильтр товара'
+        verbose_name_plural = 'Фильтры товара'
+        db_table_comment = 'Filter Goods'
+        db_table = 'filter_goods'
 
     @staticmethod
     def dictionary_fields():
         return SettingsDictionary.dictionary_fields() + [
             {
-                'field': 'hex',
-                'type': 'string',
-                'label': 'HEX',
+                'field': 'goods',
+                'type': 'foreign',
+                'label': 'товар',
+                'foreignClass': 'Goods',
+                'null': False
+            },
+            {
+                'field': 'filter_option',
+                'type': 'foreign',
+                'label': 'фильтр',
+                'foreignClass': 'FiterOption',
+                'null': False
+            },
+        ]
+
+
+class FilterToGoodsGroup(SettingsDictionary):
+    """ goods group filter set """
+    goods_group = models.ForeignKey(GoodsGroup, on_delete=models.CASCADE)
+    filter_option = models.ForeignKey(FilterOption, on_delete=models.CASCADE)
+
+    class Meta(SettingsDictionary.Meta):
+        verbose_name = 'Фильтр группы товара'
+        verbose_name_plural = 'Фильтры группы товара'
+        db_table_comment = 'Filter Goods Group'
+        db_table = 'filter_goods_group'
+
+    @staticmethod
+    def dictionary_fields():
+        return SettingsDictionary.dictionary_fields() + [
+            {
+                'field': 'goods_group',
+                'type': 'foreign',
+                'label': 'товар',
+                'foreignClass': 'GoodsGroup',
+                'null': False
+            },
+            {
+                'field': 'filter_option',
+                'type': 'foreign',
+                'label': 'фильтр',
+                'foreignClass': 'FiterOption',
                 'null': False
             },
         ]
