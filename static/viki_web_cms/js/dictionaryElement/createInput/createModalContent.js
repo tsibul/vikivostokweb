cee5efbd2cecf7288dad88fa0e203eff9ac8bf93
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-14 01:22:04 +0300

    almost ready

--- static/viki_web_cms/js/dictionaryElement/createInput/createModalContent.js
+++ static/viki_web_cms/js/dictionaryElement/createInput/createModalContent.js
@@ -1,5 +1,24 @@
 'use strict'
 
+import {createStringField} from "./createStringField.js";
+import {createBooleanField} from "./createBooleanField.js";
+import {createNumberField} from "./createNumberField.js";
+import {createForeignField} from "./createForeignField.js";
+import {createImageField} from "./createImageField.js";
+import {getFieldStructure} from "../getFieldStructure.js";
+import {jsonUrl} from "../../main.js";
+import {fetchJsonData} from "../../fetchJsonData.js";
+import {createFileField} from "./createFileField.js";
+
+const fieldCreation = {
+    'string': createStringField,
+    'boolean': createBooleanField,
+    'number': createNumberField,
+    'foreign': createForeignField,
+    'image': createImageField,
+    'file': createFileField,
+};
+
 /**
  * create editable fields for modal
  * @param modal
@@ -10,5 +29,20 @@
 export async function createModalContent(modal, className, elementId) {
     const modalContent = document.createElement("div");
     modalContent.classList.add('modal__content');
+    const titleObject = await getFieldStructure(className);
+    let tmpField;
+    const url = jsonUrl + 'record_info/' + className + '/' + elementId;
+    const recordInfo = await fetchJsonData(url);
+    if (elementId !== '0') {
+        titleObject.forEach(field => {
+            tmpField = fieldCreation[field.type](field, recordInfo.record[field.field], recordInfo.url);
+            modalContent.appendChild(tmpField);
+        });
+    } else {
+        titleObject.forEach(field => {
+            tmpField = fieldCreation[field.type](field, null, recordInfo.url);
+            modalContent.appendChild(tmpField);
+        });
+    }
     return modalContent;
 }
