e9b6ae5bd34b95d5534aa696ca8fbc789af72c32
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-14 01:22:26 +0300

    almost ready

--- static/viki_web_cms/js/dictionaryElement/createInput/createBooleanField.js
+++ static/viki_web_cms/js/dictionaryElement/createInput/createBooleanField.js
@@ -1,5 +1,13 @@
 'use strict'
 
-export function createBooleanField(fieldName) {
+import {createFieldFrame} from "./createFieldFrame.js";
+import {createModalInput} from "./createModalInput.js";
 
+export function createBooleanField(fieldName, fieldValue) {
+  const fieldFrame = createFieldFrame(fieldName);
+  const input = createModalInput('checkbox');
+  input.checked = fieldValue;
+  input.id = fieldName.field +'Input';
+  fieldFrame.appendChild(input);
+  return fieldFrame;
 }
