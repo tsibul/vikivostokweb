bc97daecf3ea5299dcba3995f973ea8e76d94a98
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-14 01:23:17 +0300

    TODO add file field

--- static/viki_web_cms/js/dictionaryElement/createInput/createImageField.js
+++ static/viki_web_cms/js/dictionaryElement/createInput/createImageField.js
@@ -1,5 +1,14 @@
 'use strict'
 
-export function createImageField(fieldName) {
+import {createFieldFrame} from "./createFieldFrame.js";
+import {createModalInput} from "./createModalInput.js";
+
+export function createImageField(fieldName, fieldValue, url) {
+    const fieldFrame = createFieldFrame(fieldName);
+    const input = createModalInput('image');
+    input.src = url + fieldValue;
+    input.alt = fieldName.field;
+    fieldFrame.appendChild(input);
+    return fieldFrame;
 
 }
