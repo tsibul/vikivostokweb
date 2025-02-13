df2e021ed5e7104ac699533014f3d7428e32ac95
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-14 01:23:31 +0300

    TODO add dropdown

--- static/viki_web_cms/js/dictionaryElement/createInput/createForeignField.js
+++ static/viki_web_cms/js/dictionaryElement/createInput/createForeignField.js
@@ -1,5 +1,14 @@
 'use strict'
 
-export function createForeignField(fieldName) {
+import {createFieldFrame} from "./createFieldFrame.js";
+
+export function createForeignField(fieldName, fieldValue, url) {
+  const fieldFrame = createFieldFrame(fieldName);
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.classList.add('modal__content_text');
+  input.value = fieldValue;
+  fieldFrame.appendChild(input);
+  return fieldFrame;
 
 }
