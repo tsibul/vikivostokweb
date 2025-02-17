de7a35eea21271d74d9eddfc631b3af79c9cd116
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-17 08:02:38 +0300

    dictionary edit initial commit

--- viki_web_cms/urls.py
+++ viki_web_cms/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('json/field_values/<str:class_name>/<int:deleted>/<int:first_record>/<str:search_string>', views.field_values),
     path('json/record_info/<str:class_name>/<int:record_id>', views.record_info),
     path('json/dropdown_list/<str:class_name>', views.dropdown_list),
+    path('json/edit_dictionary/<str:class_name>', views.edit_dictionary),
 
 ]
