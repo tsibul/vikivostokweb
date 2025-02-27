cc715e3ffd928a1c9388aea84a28fc8d257e21dd
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-28 00:55:42 +0300

    write colors in one input as json.stringify

--- static/viki_web_cms/js/catalogueElement/createColors.js
+++ static/viki_web_cms/js/catalogueElement/createColors.js
@@ -8,13 +8,14 @@ import {createCatalogueItem} from "./createCatalogueItem.js";
  * @param colors
  */
 export function createColors(item, colors) {
-    item.forEach(color => {
-        let counter = 1;
-        let position;
-        counter++;
-        colors.appendChild(createCatalogueItem('number', counter + '_color__id', color.color__id))
-        position = createCatalogueItem('number', counter + '_color_position', color.color_position);
-        position.hidden = true;
-        colors.appendChild(position);
-    });
+    // item.forEach(color => {
+    //     let counter = 1;
+    //     let position;
+    //     counter++;
+    //     colors.appendChild(createCatalogueItem('number', counter + '_color__id', color.color__id))
+    //     position = createCatalogueItem('number', counter + '_color_position', color.color_position);
+    //     position.hidden = true;
+    //     colors.appendChild(position);
+    // });
+    colors.value = JSON.stringify(item);
 }
