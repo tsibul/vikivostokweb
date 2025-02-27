a201d703a560d1ff57e8bfb60911e84d6164ab91
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-02-28 01:09:38 +0300

    change grid to make option

--- static/viki_web_cms/js/goodsElement/createGoodsElement.js
+++ static/viki_web_cms/js/goodsElement/createGoodsElement.js
@@ -3,7 +3,7 @@
 import {createDictionaryContent} from "../dictionaryElement/showDictionary/createDictionaryContent.js";
 
 export async function createGoodsElement(className){
-    const rowGrid = '14px 4fr 1fr 1.5fr 1fr 3fr 3fr 3fr 3fr 1fr 1fr 3fr 1.5fr'
+    const rowGrid = '14px 4fr 1fr 1.5fr 1fr 3fr 3fr 3fr 2fr 2fr 1fr 1fr 3fr 1.5fr'
     const dictionaryContent = await createDictionaryContent(className, rowGrid, 0, 'None');
     dictionaryContent.id = className;
     dictionaryContent.dataset.grid = rowGrid;
