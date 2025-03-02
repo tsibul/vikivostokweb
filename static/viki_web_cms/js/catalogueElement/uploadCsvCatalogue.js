ab266790ba9ff687ce94039c8b5b026a0b8f02a3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-02 08:39:09 +0300

    load csvAlert initial commit

--- static/viki_web_cms/js/catalogueElement/uploadCsvCatalogue.js
+++ static/viki_web_cms/js/catalogueElement/uploadCsvCatalogue.js
@@ -1,15 +1,42 @@
 'use strict'
 
+import {jsonUrl} from "../main.js";
+import {closeModal} from "../modalFunction/closeModal.js";
+import {reloadCatalogue} from "./reloadCatalogue.js";
+import {loadCsvAlert} from "./loadCsvAlert.js";
+
 export async function uploadCsvCatalogue(e) {
     e.preventDefault();
+    const section = document.querySelector('.content')
     const form = e.target.closest('form');
+    const errorMessage = form.querySelector('.error-message')
+    const modal = e.target.closest('dialog');
+    ;
     const formData = new FormData(form);
     const fileName = Object.fromEntries(formData.entries()).csv_file.name;
-    if (!fileName.endsWith('.csv')){
+    if (!fileName.endsWith('.csv')) {
         form.querySelector('input').value = '';
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'Неправильный формат файла';
     } else {
-    console.log(fileName);
-
+        const saveUrl = jsonUrl + 'catalogue_csv_load';
+        await fetch(saveUrl, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(rowData => {
+                const message = rowData.message;
+                if (rowData.error) {
+                    form.querySelector('input').value = '';
+                    errorMessage.style.display = 'block';
+                    errorMessage.textContent = rowData.error;
+                } else {
+                    closeModal(modal);
+                    reloadCatalogue(section, 0, 'None');
+                    loadCsvAlert(message);
+                }
+            });
     }
     console.log('mmm');
 
