25b93185e20b6b76dd688982dd23e03603b8aa33
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-05 07:17:05 +0300

    change modal open btn from save to neutral style

--- static/viki_web_cms/js/priceElement/createPriceElement.js
+++ static/viki_web_cms/js/priceElement/createPriceElement.js
@@ -2,15 +2,15 @@
 
 import {createDropDown} from "../dropDown/createDropDown.js";
 import {reloadPriceList} from "./reloadPriceList.js";
-import {createSaveButton} from "../createStandardElements/createSaveButton.js";
 import {newPriceDate} from "./newPriceDate.js";
+import {createNeutralButton} from "../createStandardElements/createNeutralButton.js";
 
 export async function createPriceElement(className){
     const dictionaryHeaderLeft = document.querySelector('.dictionary-frame__header_left');
     const priceDropdown = await createDropDown('Price', '', false);
     dictionaryHeaderLeft.appendChild(priceDropdown);
     const dropDownInput = priceDropdown.querySelector('input');
-    const newPriceBtn = createSaveButton('Новый прайс');
+    const newPriceBtn = createNeutralButton('Новый прайс');
     dictionaryHeaderLeft.appendChild(newPriceBtn);
     dropDownInput.addEventListener('change', reloadPriceList);
     newPriceBtn.addEventListener('click', newPriceDate);
