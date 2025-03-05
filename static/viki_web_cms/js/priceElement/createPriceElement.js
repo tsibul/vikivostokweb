b5998ec75b67a85a9422083fd05ace9ae5d5f97b
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-05 09:43:42 +0300

    change dropDown to priceDropdown.js

--- static/viki_web_cms/js/priceElement/createPriceElement.js
+++ static/viki_web_cms/js/priceElement/createPriceElement.js
@@ -1,13 +1,13 @@
 'use strict'
 
-import {createDropDown} from "../dropDown/createDropDown.js";
 import {reloadPriceList} from "./reloadPriceList.js";
 import {newPriceDate} from "./newPriceDate.js";
 import {createNeutralButton} from "../createStandardElements/createNeutralButton.js";
+import {createPriceDropdown} from "./priceDropdown.js";
 
 export async function createPriceElement(className){
     const dictionaryHeaderLeft = document.querySelector('.dictionary-frame__header_left');
-    const priceDropdown = await createDropDown('Price', '', false);
+    const priceDropdown = await createPriceDropdown();
     dictionaryHeaderLeft.appendChild(priceDropdown);
     const dropDownInput = priceDropdown.querySelector('input');
     const newPriceBtn = createNeutralButton('Новый прайс');
