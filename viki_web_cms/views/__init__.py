6c308bfdd8c0405323f0f04ede0949b9a29cef7a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-05 13:03:10 +0300

    add price views

--- viki_web_cms/views/__init__.py
+++ viki_web_cms/views/__init__.py
@@ -3,3 +3,4 @@ from viki_web_cms.views.dictonary_fields import *
 from viki_web_cms.views.dictionary_edit import *
 from viki_web_cms.views.catalogue import *
 from viki_web_cms.views.catalogue_csv import *
+from viki_web_cms.views.price import *
