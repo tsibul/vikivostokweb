e30edffcd8a2de257ac184cf36cbc7b98900e646
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-06 11:02:39 +0300

    add float field type

--- static/viki_web_cms/js/dictionaryElement/showDictionary/gridDictonaryStyle.js
+++ static/viki_web_cms/js/dictionaryElement/showDictionary/gridDictonaryStyle.js
@@ -4,7 +4,7 @@ export function gridDictionaryStyle(fields) {
     let elementStile = '14px 6fr ';
     fields.forEach(field => {
         if (field.field !== 'name') {
-            field.type === 'boolean' || field.type === 'number'
+            field.type === 'boolean' || field.type === 'number' || field.type === 'float'
                 ? elementStile = elementStile + '1fr '
                 : elementStile = elementStile + '3fr ';
         }
