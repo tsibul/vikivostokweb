a36077c0eac66a8e7aa3956cae47cd611802a823
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-08 02:30:22 +0300

    initial commit

--- static/viki_web_cms/js/priceElement/reloadPriceList.js
+++ static/viki_web_cms/js/priceElement/reloadPriceList.js
@@ -1,5 +1,19 @@
 'use strict'
 
-export async function reloadPriceList(e) {
+import {createPriceContent} from "./createPriceContent.js";
 
+export async function reloadPriceList(e) {
+    const content = e.target.closest('.content');
+    const priceContent = content.querySelector('.price-block');
+    const priceDate = content
+        .querySelector('.dictionary-frame__header_left')
+        .querySelector('.price-dropdown__input').value;
+    const searchString = content.querySelector('.dictionary-frame__input').value;
+    const priceType = content
+        .querySelector('.dictionary-frame__header_right')
+        .querySelector('input[hidden]').value;
+    priceContent.innerHTML = '';
+    const priceBlocks = await createPriceContent(priceDate, priceType, searchString);
+    priceContent.appendChild(priceBlocks.header);
+    priceContent.appendChild(priceBlocks.form);
 }
