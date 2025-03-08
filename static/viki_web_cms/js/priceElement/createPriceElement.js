10a50e3b5b7fe4f4096f09f64f8baac02587518d
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-08 08:50:32 +0300

    recreate search elements in order to change event listeners

--- static/viki_web_cms/js/priceElement/createPriceElement.js
+++ static/viki_web_cms/js/priceElement/createPriceElement.js
@@ -6,8 +6,9 @@ import {createNeutralButton} from "../createStandardElements/createNeutralButton
 import {createPriceDropdown, priceDropdownBody} from "./priceDropdownBody.js";
 import {createPriceContent} from "./createPriceContent.js";
 import {createCancelButton} from "../createStandardElements/createCancelButton.js";
+import {createSaveButton} from "../createStandardElements/createSaveButton.js";
 
-export async function createPriceElement(className){
+export async function createPriceElement(className) {
     const dictionaryHeaderLeft = document.querySelector('.dictionary-frame__header_left');
     const priceDropdown = await createPriceDropdown();
     dictionaryHeaderLeft.appendChild(priceDropdown);
@@ -35,13 +36,37 @@ export async function createPriceElement(className){
     priceTypeDropdown.id = "priceType";
     priceTypeDropdown.addEventListener('change', await reloadPriceList);
     dictionaryHeaderRight.insertAdjacentElement('afterbegin', priceTypeDropdown);
-    dictionaryHeaderRight.insertAdjacentElement('afterbegin',priceTypeLabel);
+    dictionaryHeaderRight.insertAdjacentElement('afterbegin', priceTypeLabel);
     const priceContent = document.createElement("div");
     priceContent.classList.add('price-block');
     const priceTypeId = priceTypeDropdown.querySelector('input[hidden]').value;
     const priceBlocks = await createPriceContent(dropDownInput.value, priceTypeId, 'None')
     priceContent.appendChild(priceBlocks.header);
     priceContent.appendChild(priceBlocks.form);
+
+    const searchInputOld = dictionaryHeaderRight.querySelector('.dictionary-frame__input');
+    searchInputOld.remove();
+    const searchBtnOld = dictionaryHeaderRight.querySelector('.btn__save');
+    searchBtnOld.remove()
+    const cancelSearchBtnOld = dictionaryHeaderRight.querySelector('.btn__cancel');
+    cancelSearchBtnOld.remove();
+    const searchInput = document.createElement('input');
+    searchInput.classList.add('dictionary-frame__input');
+    searchInput.type = 'text';
+    searchInput.placeholder = 'поиск...';
+    searchInput.addEventListener('keypress', e => {
+        if (e.key === 'Enter' && e.target.value) reloadPriceList();
+    })
+    dictionaryHeaderRight.appendChild(searchInput);
+    const searchBtn = createSaveButton('Поиск');
+    searchBtn.addEventListener('click', reloadPriceList);
+    dictionaryHeaderRight.appendChild(searchBtn);
+    const clearBtn = createCancelButton('Очистить');
+    clearBtn.addEventListener('click', e => {
+        searchInput.value = '';
+        reloadPriceList();
+    })
+    dictionaryHeaderRight.appendChild(clearBtn);
     return priceContent;
 }
 
