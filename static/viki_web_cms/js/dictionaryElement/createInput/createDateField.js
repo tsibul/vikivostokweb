c62289defd6d20b54ee2bfdffda066a2b4044316
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-09 14:14:47 +0300

    initial commit

---
+++ static/viki_web_cms/js/dictionaryElement/createInput/createDateField.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import {createModalInput} from "./createModalInput.js";
+
+/**
+ * create number input
+ * @returns {HTMLInputElement}
+ * @param fieldData
+ */
+export function createDateField(fieldData) {
+    const input = createModalInput('date');
+    input.value = fieldData.fieldValue;
+    input.name = fieldData.fieldName.field;
+    return input;
+}
