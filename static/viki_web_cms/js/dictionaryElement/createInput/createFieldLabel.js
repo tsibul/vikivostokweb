40ca343b652457163685b26235da8bb0fe6d32d7
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-14 09:43:53 +0300

    add * on necessary fields

--- static/viki_web_cms/js/dictionaryElement/createInput/createFieldLabel.js
+++ static/viki_web_cms/js/dictionaryElement/createInput/createFieldLabel.js
@@ -2,13 +2,13 @@
 
 /**
  * create frame field label + field input
- * @param text
+ * @param fieldData
  * @returns {HTMLDivElement}
  */
-export function createFieldLabel(text) {
+export function createFieldLabel(fieldData) {
     const fieldLabel = document.createElement('label');
     fieldLabel.classList.add('modal__content_label');
-    fieldLabel.htmlFor = text.field + 'Input';
-    fieldLabel.textContent = text.label;
+    fieldLabel.htmlFor = fieldData.field + 'Input';
+    fieldLabel.textContent = fieldData.label + (fieldData['null'] ? '' : '*');
     return fieldLabel;
 }
