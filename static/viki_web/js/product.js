0928e1d0d4570bd67533bce9cebc184b1d1c1ebc
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-16 15:01:48 +0300

    add color label & tab btn

--- static/viki_web/js/product.js
+++ static/viki_web/js/product.js
@@ -1,6 +1,12 @@
 'use strict'
 
-const colorInputs = document.querySelectorAll('.product-header__filter-content_checkbox-hidden');
+import {selectItemColor} from "./product/selectItemColor.js";
+import {changeDetailTab} from "./product/changeDetailTab.js";
+
+const colorInputs = document.querySelectorAll('.product-header .product-header__filter-content_checkbox-hidden');
+const productColorLabels  = document
+    .querySelectorAll('.products label.color-label');
+const detailTabButtons = document.querySelectorAll('.product-hor__tab-btn');
 
 
 colorInputs.forEach(colorInput => {
@@ -9,5 +15,13 @@ colorInputs.forEach(colorInput => {
             .querySelector(`label[for="${colorInput.id}"]`)
             .querySelector('.color-label__check');
         colorInput.checked ? label.style.display = 'block': label.style.display = 'none';
-    })
+    });
+});
+
+productColorLabels.forEach(colorLabel => {
+    colorLabel.addEventListener('click', selectItemColor);
+});
+
+detailTabButtons.forEach(tab => {
+    tab.addEventListener('click', changeDetailTab);
 })
