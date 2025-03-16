acc18ecdcfbb2bf9578e52ccf8a5e25f9ede818d
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-16 16:25:35 +0300

    add article_description, goods_description

--- viki_web/views/product.py
+++ viki_web/views/product.py
@@ -1,9 +1,10 @@
 import random
+from nntplib import ArticleInfo
 
 from django.shortcuts import render
 
 from viki_web_cms.models import ProductGroup, Goods, CatalogueItem, ColorGroup, FilterToGoodsGroup, GoodsGroup, \
-    PrintType
+    PrintType, GoodsDescription, ArticleDescription, CatalogueItemColor
 
 
 def product(request, product_group_url):
@@ -26,7 +27,23 @@ def product(request, product_group_url):
     )
     goods_list = []
     for goods_item in goods:
+        description = GoodsDescription.objects.filter(
+            goods=goods_item, deleted=False).first()
+        goods_description = description.description if description else ''
         items = CatalogueItem.objects.filter(goods=goods_item, deleted=False)
+        article_description = ArticleDescription.objects.filter(deleted=False, goods=goods_item)
+        item_list = []
+        for item in items:
+            item_colors = CatalogueItemColor.objects.filter(deleted=False, item=item)
+            color_description = ''
+            if len(article_description):
+                for description in article_description:
+                    color_description += (description.parts_description.name.upper() + ': ' +
+                                          item_colors.get(color_position=description.position).color.name + ', ')
+            item_list.append({
+                'item': item,
+                'color_description': color_description
+            })
         colors = items.values(
             'id',
             'main_color__hex'
@@ -38,10 +55,11 @@ def product(request, product_group_url):
             id_random = id_list[0]
         goods_list.append({
             'goods_item': goods_item,
-            'items': items,
+            'item_list': item_list,
             'item_id': id_list,
             'colors': colors,
             'id_random': id_random,
+            'goods_description': goods_description,
         })
     context = {
         'product_groups': product_groups,
@@ -49,7 +67,7 @@ def product(request, product_group_url):
         'color_group': color_group,
         'filter_option': filter_option,
         'print_types': print_types,
-        'goods': goods_list,
+        'goods_list': goods_list,
     }
     if product_group.layout.id == 1:
         return render(request, 'product_hor.html', context)
