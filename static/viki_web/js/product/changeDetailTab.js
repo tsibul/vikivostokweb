6fa8c29d8c576e6149cf80b5d31716ec1a86a291
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-16 15:00:07 +0300

    initial commit

---
+++ static/viki_web/js/product/changeDetailTab.js
@@ -0,0 +1,18 @@
+'use strict'
+
+export function changeDetailTab(e) {
+    const tabs = e.target.closest('.product-hor__tab-btn-block');
+    const tabContent = tabs.closest('.product-hor__frame')
+        .querySelector('.product-hor__tab-content');
+    const contents = tabContent.querySelectorAll('.product-hor__tab');
+    const newContent = [...contents].find( content => content.dataset.id === e.target.dataset.id);
+    const tabBtn = tabs.querySelectorAll('.product-hor__tab-btn');
+    tabBtn.forEach(btn => {
+        btn.classList.remove('tab-active');
+    });
+    e.target.classList.add('tab-active');
+    contents.forEach(content => {
+        content.classList.remove('tab-show');
+    });
+    newContent.classList.add('table-show');
+}
