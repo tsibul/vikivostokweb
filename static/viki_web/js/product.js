766ba8f1da3253bd433b2e0a1bb7e97a53c8e2e3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-21 07:44:13 +0300

    split product js

--- static/viki_web/js/product.js
+++ static/viki_web/js/product.js
@@ -1,37 +1,48 @@
 'use strict'
 
-import {selectItemColor} from "./product/selectItemColor.js";
-import {changeDetailTab} from "./product/changeDetailTab.js";
-import {selectNext} from "./product/selectNext.js";
-import {moveRange} from "./product/moveRange.js";
+// import {selectItemColor} from "./product/selectItemColor.js";
+// import {changeDetailTab} from "./product/changeDetailTab.js";
+// import {selectNext} from "./product/selectNext.js";
+// import {moveRange} from "./product/moveRange.js";
+// import {clearFilter} from "./product/clearFilter.js";
+// import {applyFilter} from "./product/applyFilter.js";
+import {productListeners} from "./product/productListeners.js";
 
-const colorInputs = document.querySelectorAll('.filter .product-header__filter-content_checkbox-hidden');
-const productColorLabels  = document
-    .querySelectorAll('.products label.color-label');
-const detailTabButtons = document.querySelectorAll('.product-hor__tab-btn');
-const chevronNext = document.querySelectorAll('.chevron-next');
-const priceRange = document.querySelector('.filter .input-range');
+await productListeners();
 
 
-colorInputs.forEach(colorInput => {
-    colorInput.addEventListener('change', (e) => {
-        const label = document
-            .querySelector(`label[for="${colorInput.id}"]`)
-            .querySelector('.color-label__check');
-        colorInput.checked ? label.style.display = 'block': label.style.display = 'none';
-    });
-});
-
-productColorLabels.forEach(colorLabel => {
-    colorLabel.addEventListener('click', selectItemColor);
-});
-
-detailTabButtons.forEach(tab => {
-    tab.addEventListener('click', changeDetailTab);
-})
-
-chevronNext.forEach( chev => {
-    chev.addEventListener('click', selectNext);
-});
-
-priceRange.addEventListener('input', moveRange)
+// const colorInputs = document.querySelectorAll('.filter .product-header__filter-content_checkbox-hidden');
+// const productColorLabels  = document
+//     .querySelectorAll('.products label.color-label');
+// const detailTabButtons = document.querySelectorAll('.product-hor__tab-btn');
+// const chevronNext = document.querySelectorAll('.chevron-next');
+// const priceRange = document.querySelector('.filter .input-range');
+// const filterSubmit = document.querySelector('.filter .btn__save');
+// const filterCancel = document.querySelector('.filter .btn__cancel');
+//
+// colorInputs.forEach(colorInput => {
+//     colorInput.addEventListener('change', (e) => {
+//         const label = document
+//             .querySelector(`label[for="${colorInput.id}"]`)
+//             .querySelector('.color-label__check');
+//         colorInput.checked ? label.style.display = 'block': label.style.display = 'none';
+//     });
+// });
+//
+// productColorLabels.forEach(colorLabel => {
+//     colorLabel.addEventListener('click', selectItemColor);
+// });
+//
+// detailTabButtons.forEach(tab => {
+//     tab.addEventListener('click', changeDetailTab);
+// })
+//
+// chevronNext.forEach( chev => {
+//     chev.addEventListener('click', selectNext);
+// });
+//
+// priceRange.addEventListener('input', moveRange);
+//
+// filterCancel.addEventListener('click', clearFilter);
+//
+// filterSubmit.addEventListener('click', await applyFilter);
