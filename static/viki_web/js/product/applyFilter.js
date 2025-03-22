1efe2e389e81def25304fb65f224bed804170c97
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-22 17:22:54 +0300

    add price filter

--- static/viki_web/js/product/applyFilter.js
+++ static/viki_web/js/product/applyFilter.js
@@ -1,7 +1,5 @@
 'use strict'
 
-import {productListeners} from "./productListeners.js";
-
 export async function applyFilter(e) {
     e.preventDefault()
     const postUrl = `./${e.target.dataset.url}`;
@@ -47,7 +45,7 @@ export async function applyFilter(e) {
     const priceInput = filterDetails.querySelector(`.filter .input-range`);
     if (priceInput.value <= priceInput.max - 0.1) {
         priceIdList =
-            await priceFilterList(priceInput.value, Object.keys(goodsIds), postUrl, 'price')
+            priceFilterList(priceInput.value, allItems)
     }
 
     combineFilters(filterIdList, printIdList, colorIdList, priceIdList, goodsIds, allGoods, itemIds, allItems);
@@ -56,17 +54,20 @@ export async function applyFilter(e) {
 
 function combineFilters(filterIdList, printIdList, colorIdList, priceIdList, goodsIds, allGoods, itemIds, allItems) {
 
-    let filterTemp, printTemp, colorTemp, colorItemTemp
+    let filterTemp, printTemp, colorTemp, priceTemp, colorItemTemp, priceItemTemp;
     const filteredGoods = [];
     const filteredItems = []
-    filterIdList ? filterTemp = filterIdList : filterTemp = Object.keys(goodsIds);
+    filterTemp = filterIdList ? filterIdList : Object.keys(goodsIds);
     printTemp = printIdList ? printIdList : Object.keys(goodsIds);
     colorTemp = colorIdList ? colorIdList['goods'] : Object.keys(goodsIds);
-    colorItemTemp = colorIdList && colorIdList['item'] ? colorIdList['item'] : Object.keys(goodsIds);
+    colorItemTemp = colorIdList && colorIdList['item'] ? colorIdList['item'] : Object.keys(itemIds);
+    priceTemp = priceIdList ? priceIdList['goods'] : Object.keys(goodsIds);
+    priceItemTemp = priceIdList && priceIdList['item'] ? priceIdList['item'] : Object.keys(itemIds);
     Object.keys(goodsIds).forEach(goodsKey => {
-        if (!filterTemp.includes(goodsKey) ||
-            !printTemp.includes(goodsKey) ||
-            !colorTemp.includes(goodsKey)
+        if (!filterTemp.includes(goodsKey)
+            || !printTemp.includes(goodsKey)
+            || !colorTemp.includes(goodsKey)
+            || !priceTemp.includes(goodsKey)
         ) {
             goodsIds[goodsKey].setAttribute('style', 'display:none');
         } else {
@@ -74,9 +75,13 @@ function combineFilters(filterIdList, printIdList, colorIdList, priceIdList, goo
             filteredGoods.push(goodsIds[goodsKey]);
         }
     });
-    if (colorIdList) {
+    if (colorIdList
+        || priceIdList
+    ) {
         Object.keys(itemIds).forEach(itemKey => {
-            if (!colorItemTemp.includes(itemKey)) {
+            if (!colorItemTemp.includes(itemKey)
+                || !priceItemTemp.includes(itemKey)
+            ) {
                 itemIds[itemKey].setAttribute('style', 'display:none');
             } else {
                 itemIds[itemKey].removeAttribute('style');
@@ -86,7 +91,9 @@ function combineFilters(filterIdList, printIdList, colorIdList, priceIdList, goo
         const allColors = document.querySelectorAll(`.products .color-label`);
         [...allColors].forEach(colorLabel => {
             colorLabel.removeAttribute('style');
-            if (!colorItemTemp.includes(colorLabel.dataset.itemId)) {
+            if (!colorItemTemp.includes(colorLabel.dataset.itemId)
+            || !priceItemTemp.includes(colorLabel.dataset.itemId)
+            ) {
                 colorLabel.setAttribute('style', 'display:none');
             }
         });
@@ -102,7 +109,7 @@ function combineFilters(filterIdList, printIdList, colorIdList, priceIdList, goo
                     item.classList.add('item-hidden', 'item-opaque');
                 }
             });
-        })
+        });
     }
 
 }
@@ -131,45 +138,21 @@ async function goodsFilterList(filterInputs, goodsIds, postUrl, filterType) {
     return data['idList'];
 }
 
-async function priceFilterList(priceLimit, goodsIds, postUrl) {
-    const itemFrame = document.querySelectorAll('.product-hor__frame');
-
+function priceFilterList(priceLimit, allItems) {
+    const goodsList = [];
+    const itemList = [];
+    [...allItems].forEach(item => {
+        if (parseFloat(item.dataset.price) <= parseFloat(priceLimit)) {
+            itemList.push(item.dataset.id);
+            if (!goodsList.includes(item.dataset.goods)) {
+                goodsList.push(item.dataset.goods);
+            }
+        }
+    });
+    return {
+        'goods': goodsList,
+        'item': itemList,
+    };
 }
 
-
-// export async function applyFilter(e) {
-//     const filterDetails = document.querySelector(`.filter`);
-//     const filterInputs = filterDetails
-//         .querySelectorAll(`.filter__content_data_filter input[type="checkbox"]`);
-//     const printInputs = filterDetails
-//         .querySelectorAll(`.filter__content_data_print input[type="checkbox"]`);
-//     const colorInputs = filterDetails
-//         .querySelectorAll(`.filter__content_data_color input[type="checkbox"]`);
-//     const priceInput = filterDetails.querySelector(`.filter .input-range`);
-//     if (colorInputs.length > 0 ||
-//         filterInputs.length > 0 ||
-//         colorInputs.length > 0 ){
-//         const postData = {
-//             'filter': checkedFilter(filterInputs),
-//             'print_type': checkedFilter(printInputs),
-//             'color': checkedFilter(colorInputs),
-//             'price': priceInput.value,
-//         }
-//         const postUrl = `./${e.target.dataset.url}/filter`
-//         const response = await fetch(postUrl, {
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify(postData),
-//         });
-//         const text = await response.text();
-//         document.open();
-//         document.write(text);
-//         document.close();
-//         await productListeners();
-//     } else {
-//             filterDetails.removeAttribute('open')
-//     }
-// }
 
