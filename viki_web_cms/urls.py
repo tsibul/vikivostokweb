d3cca154eebc7aebce540c01301c563213eb7267
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-30 00:18:23 +0300

    customer dictionaries

--- viki_web_cms/urls.py
+++ viki_web_cms/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'viki_web_cms'
 urlpatterns = [
     path('', views.main_cms, name='main'),
+    path('customer', views.main_customer, name='main_customer'),
+
     path('json/field_names/<str:class_name>', views.field_names),
     path('json/field_values/<str:class_name>/<int:deleted>/<int:first_record>/<str:search_string>', views.field_values),
     path('json/record_info/<str:class_name>/<int:record_id>', views.record_info),
