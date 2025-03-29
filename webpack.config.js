327841e3f255b7084424389a5d50c99fdba7e558
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-29 08:25:17 +0300

    new config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    cms: './static/viki_web_cms/js/main.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'static/viki_web_cms/distribute'),
-    clean: true,
-  },
-  mode: "production",
-};
+module.exports = [{
+    entry: {
+        cms: './static/viki_web_cms/js/main.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'static/viki_web_cms/distribute'),
+        clean: true,
+    },
+    mode: "production",
+},
+    {
+        entry: {
+            login: './static/viki_web/js/login.js',
+            header: './static/viki_web/js/header.js',
+            product: './static/viki_web/js/product.js',
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'static/viki_web/distribute'),
+            clean: true,
+        },
+        mode: "production",
+    },
+];
