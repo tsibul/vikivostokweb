f76c546f7587bb3a93be8d96035899b3cedd878b
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-03-30 23:20:03 +0300

    initial commit

---
+++ static/viki_web/js/cabinet/dataSave.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export async function dataSave(data) {
+
+}
---
+++ static/viki_web/js/cabinet/startEdit.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export function startEdit(data) {
+    const dataChange = data.querySelector('.personal-data__change');
+    const dataSave = data.querySelector('.personal-data__save');
+    dataChange.classList.toggle('item-hidden');
+    dataSave.classList.toggle('item-hidden');
+    const dataDisabled = data.querySelectorAll('.input-disabled');
+    [...dataDisabled].forEach(input => {
+        input.disabled = false;
+    });
+
+}
