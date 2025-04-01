2be8e1e7c1835ef62038e93867a4ce5a885c4de3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-01 19:56:38 +0300

    disable inputs after personal and legal data

--- static/viki_web/js/cabinet.js
+++ static/viki_web/js/cabinet.js
@@ -1,16 +1,20 @@
 'use strict';
 
-import {dataInitial} from "./cabinet/dataInitial.js";
 import {allInputDisabled} from "./cabinet/allInputDisabled.js";
 import {editBtnListeners} from "./cabinet/editBtnListeners.js";
+import {fullDataInitial} from "./cabinet/fullDataInitial.js";
 
 const allInputs = document.querySelectorAll('input.input-disabled');
 const personalData = document.querySelector('.personal-data');
+const legalData = document.querySelector('.legal-data');
 
 document.querySelector('li.log-logout').style.display='none'
+
+const dataInitial = await fullDataInitial(personalData, legalData);
+const personalDataInitial = dataInitial['personalData']
+
 allInputDisabled();
 
-const personalDataInitial = await dataInitial(personalData);
 await editBtnListeners(personalData, personalDataInitial, '#first_name', 'personal');
 
 [...allInputs].forEach(item => {
