193cefa05dbc079af252a90e2c70e159f54d0d25
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-04 08:22:00 +0300

    add listeners for new bankAccount

--- static/viki_web/js/cabinet.js
+++ static/viki_web/js/cabinet.js
@@ -7,12 +7,16 @@ import {saveCompany} from "./cabinet/saveCompany.js";
 import {checkCompany} from "./cabinet/checkCompany.js";
 import {cancelDialog} from "./cabinet/cancelDialog.js";
 import {modalDnD} from "./common/modalDnD.js";
+import {checkBank} from './cabinet/checkBank.js';
+import {saveBank} from './cabinet/saveBank.js';
 
 const allInputs = document.querySelectorAll('input.input-disabled');
 const personalData = document.querySelector('.personal-data');
 const legalData = document.querySelector('.legal-data');
 export const companyDialog = document.querySelector('#company-dialog');
 export const companySaveDialog = document.querySelector('#company-dialog-save');
+export const bankDialog = document.querySelector('#bank-dialog');
+export const bankSaveDialog = document.querySelector('#bank-dialog-save');
 
 document.querySelector('li.log-logout').style.display='none'
 
@@ -41,6 +45,27 @@ companySaveDialog.querySelector('form').addEventListener('reset', async (e) => {
     cancelDialog(e, companySaveDialog);
 });
 
+// Initialize bank dialog handlers
+document.querySelector('#new-bank').addEventListener('click', (e) => {
+        const companyId = e.target.closest('.company').querySelector(`input[name="id"]`).value;
+        bankDialog.querySelector('form').dataset.companyId = companyId;
+        bankDialog.showModal();
+        modalDnD(bankDialog);
+    });
+
+bankDialog.querySelector('form').addEventListener('submit', async (e) => {
+    const companyId = e.target.dataset.companyId;
+    await checkBank(e, bankDialog, companyId);
+});
+bankDialog.querySelector('form').addEventListener('reset', (e) => {
+    cancelDialog(e, bankDialog);
+});
+bankSaveDialog.querySelector('form').addEventListener('submit', async (e) => {
+    await saveBank(e, bankSaveDialog);
+});
+bankSaveDialog.querySelector('form').addEventListener('reset', (e) => {
+    cancelDialog(e, bankSaveDialog);
+});
 
 await editBtnListeners(personalData, personalDataInitial, '#first_name', 'personal');
 
