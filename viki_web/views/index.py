79b2d4daa9c0f70e67a948ee7e27548a00b0942c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-06 14:42:07 +0300

    bugfix filter deleted=false

--- viki_web/views/index.py
+++ viki_web/views/index.py
@@ -4,6 +4,6 @@ from viki_web_cms.models import ProductGroup
 
 
 def index(request):
-    categories = ProductGroup.objects.all()
+    categories = ProductGroup.objects.filter(deleted=False)
     context = {'categories': categories, 'user': request.user}
     return render(request, 'index.html', context)
