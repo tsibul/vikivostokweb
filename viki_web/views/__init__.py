4aee23f9dd9bd134d4743f1c000bb688075a5565
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-07 07:40:00 +0300

    add product_detail.py

--- viki_web/views/__init__.py
+++ viki_web/views/__init__.py
@@ -3,3 +3,4 @@ from viki_web.views.index import *
 from viki_web.views.product import *
 from viki_web.views.product_filter import *
 from viki_web.views.cabinet import *
+from viki_web.views.product_detail import *
