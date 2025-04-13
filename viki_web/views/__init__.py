267518e7216ef8d04799070a3c65ebe0b63ca84a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-13 13:50:13 +0300

    add new_items

--- viki_web/views/__init__.py
+++ viki_web/views/__init__.py
@@ -1,6 +1,6 @@
 from viki_web.views.login import *
-from viki_web.views.index import *
 from viki_web.views.product import *
 from viki_web.views.product_filter import *
 from viki_web.views.cabinet import *
 from viki_web.views.product_detail import *
+from viki_web.views.index import *
--- viki_web/views/index.py
+++ viki_web/views/index.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
-from viki_web_cms.models import ProductGroup
+from viki_web.views import find_price_type, goods_price
+from viki_web_cms.models import ProductGroup, Goods, CatalogueItem
 
 
 def index(request):
     categories = ProductGroup.objects.filter(deleted=False)
-    context = {'categories': categories, 'user': request.user}
+    new_items_request = Goods.objects.filter(deleted=False, new=True)
+    new_items = []
+    for item in new_items_request:
+        price_type = find_price_type(request)
+        price = goods_price(item, price_type)
+        catalogue_item = CatalogueItem.objects.filter(goods=item, deleted=False).order_by('?').first()
+        if catalogue_item:
+            new_items.append({'price': price, 'image': catalogue_item.image, 'name': item.name,
+                                  'slug': item.slug})
+
+    context = {'categories': categories, 'user': request.user, 'new_items': new_items}
     return render(request, 'index.html', context)
