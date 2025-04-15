06282cfc16e48f241ed230929f4c6651b2c9e60e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-16 00:41:30 +0300

    add goods group

--- viki_web/views/catalogue.py
+++ viki_web/views/catalogue.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from transliterate import translit
 
 from viki_web.views import product_context
-from viki_web_cms.models import ProductGroup, Goods
+from viki_web_cms.models import ProductGroup, Goods, GoodsGroup
 
 
 def catalogue_search(request):
@@ -24,17 +24,31 @@ def catalogue_search(request):
             Q(deleted=False) &
             Q(catalogueitem__isnull=False)
         ).distinct()
-        context = product_context(request, goods, product_groups, None)
+        product_group = {'name': 'Каталог', 'product_group_url': 'catalogue'}
+        context = product_context(request, goods, product_groups, product_group)
         context['search']= search_string
+        # context['goods_groups']= goods_groups
         return render(request, 'product_sqr.html', context)
     else:
-        product_group_names = ProductGroup.objects.filter(deleted=False)
+        goods_groups = GoodsGroup.objects.filter(deleted=False)
         product_group = {'name': 'Каталог', 'product_group_url': 'catalogue'}
         context = {
             'user': request.user,
             'product_groups': product_groups,
-            'product_group_names': product_group_names,
             'product_group': product_group,
+            'goods_groups': goods_groups,
         }
         return render(request, 'product_sqr.html', context)
+
+def goods_group_page(request, url):
+    product_groups = ProductGroup.objects.filter(deleted=False)
+    goods_group = GoodsGroup.objects.filter(deleted=False, goods_group_url=url).first()
+    goods = Goods.objects.filter(
+        deleted=False,
+        goods_group=goods_group,
+        catalogueitem__isnull=False
+    ).distinct()
+    product_group = {'name': goods_group.name, 'product_group_url': goods_group.goods_group_url}
+    context = product_context(request, goods, product_groups, product_group)
+    return render(request, 'product_sqr.html', context)
 
