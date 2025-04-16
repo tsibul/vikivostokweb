35708acd67cb6b1c2f2966fde442f5530a64ee7f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-04-16 09:54:50 +0300

    change page style

--- viki_web/views/index.py
+++ viki_web/views/index.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from viki_web.views import find_price_type, goods_price
-from viki_web_cms.models import ProductGroup, Goods, CatalogueItem
+from viki_web_cms.models import ProductGroup, Goods, CatalogueItem, GoodsDescription
 
 
 def index(request):
@@ -12,9 +12,18 @@ def index(request):
         price_type = find_price_type(request)
         price, price_volume = goods_price(item, price_type)
         catalogue_item = CatalogueItem.objects.filter(goods=item, deleted=False).order_by('?').first()
+        description = GoodsDescription.objects.filter(
+            goods=item, deleted=False).first()
+        goods_description = description.description if description else ''
         if catalogue_item:
-            new_items.append({'price': price, 'image': catalogue_item.image, 'name': item.name,
-                                  'slug': item.slug, 'price_volume': price_volume})
+            new_items.append({
+                'price': price,
+                'image': catalogue_item.image,
+                'name': item.name,
+                'slug': item.slug,
+                'price_volume': price_volume,
+                'description': goods_description,
+            })
 
     context = {'categories': categories, 'user': request.user, 'new_items': new_items}
     return render(request, 'index.html', context)
