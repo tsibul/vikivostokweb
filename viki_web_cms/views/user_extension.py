8e1d22fac953c057f5a715eecd8e59071389f7a9
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-02 23:16:25 +0300

    bugfix with search for customer.name

--- viki_web_cms/views/user_extension.py
+++ viki_web_cms/views/user_extension.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from viki_web_cms.models.user_models import UserExtension
@@ -17,38 +18,36 @@ def user_extension(request):
     # Проверяем аутентификацию пользователя
     if user_check(request):
         return JsonResponse({"userList": [], "last_record": 0})
-    
+
     search_string = request.GET.get('search', '')
     new_filter = request.GET.get('new', '')
     last_record = int(request.GET.get('last_record', 0))
-    
+
     # Query user extensions joined with User, excluding staff
     user_query = UserExtension.objects.select_related('user', 'customer').filter(
         user__is_staff=False
     )
-    
+
     # Apply new filter if requested
     if new_filter == '1':
         user_query = user_query.filter(new=True)
-    
+
     # Apply search filter if provided
     if search_string:
         user_query = user_query.filter(
-            user__last_name__icontains=search_string
-        ) | user_query.filter(
-            user__first_name__icontains=search_string
-        ) | user_query.filter(
-            user__email__icontains=search_string
-        ) | user_query.filter(
-            phone__icontains=search_string
+            Q(user__last_name__icontains=search_string) |
+            Q(user__first_name__icontains=search_string) |
+            Q(user__email__icontains=search_string) |
+            Q(phone__icontains=search_string) |
+            Q(customer__name__icontains=search_string)
         )
-    
+
     # Order by default ordering
     user_query = user_query.order_by(*UserExtension.order_default())
-    
+
     # Paginate results (20 records per page)
-    users_data = user_query[last_record:last_record+20]
-    
+    users_data = user_query[last_record:last_record + 20]
+
     # Prepare result data
     result = []
     for user_ext in users_data:
@@ -64,7 +63,7 @@ def user_extension(request):
             'manager_letter': user_ext.manager_letter or '',
             'customer': user_ext.customer.name if user_ext.customer else ''
         })
-    
+
     return JsonResponse({
         'userList': result,
         'last_record': last_record + len(result)
