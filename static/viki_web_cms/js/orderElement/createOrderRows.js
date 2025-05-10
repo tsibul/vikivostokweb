5039ab87354eabb9581d055029570df1f949e4ea
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-10 19:06:52 +0300

    orderRow initial commit

--- static/viki_web_cms/js/orderElement/createOrderRows.js
+++ static/viki_web_cms/js/orderElement/createOrderRows.js
@@ -1,5 +1,42 @@
 'use strict';
 
-export function createOrderRows(container, lastRecord = 0, searchString = '', newOnly = false){
+import {createSaveButton} from "../createStandardElements/createSaveButton";
+import {editOrder} from "./editOrder.js";
+
+/**
+ *
+ * @param {HTMLElement} row
+ * @param {Object} order
+ */
+export function createOrderRow(row, order) {
+    let tmpField;
+    tmpField = createTextField(user.last_name);
+    row.appendChild(tmpField);
+    tmpField = createTextField(user.first_name);
+    row.appendChild(tmpField);
+    tmpField = createTextField(user.email);
+    row.appendChild(tmpField);
+    tmpField = createTextField(user.phone);
+    row.appendChild(tmpField);
+    tmpField = createTextField(user.alias);
+    tmpField.classList.add('alias')
+    row.appendChild(tmpField);
+    tmpField = createTextField(user.customer);
+    tmpField.classList.add('customer')
+    row.appendChild(tmpField);
+
+    const button = createSaveButton('Изм')
+    button.dataset.id = user.id;
+    button.addEventListener('click', async (e) => {
+        await editOrder(e, row);
+    });
+    row.appendChild(button);
+}
+
+function createTextField(text) {
+    const cell = document.createElement('div');
+    cell.classList.add('dictionary-content__row_item', 'user-element__cell');
+    cell.textContent = text || '';
+    return cell;
 
 }
---
+++ static/viki_web_cms/js/orderElement/getOrderData.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export async function getOrderData (lastRecord = 0, searchString = '', newOnly = false){
+
+}
