a76a8d253dc4ed9474ad76b42992bdc981302940
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-10 19:38:46 +0300

    add title columns

--- static/viki_web_cms/js/orderElement/createOrderElement.js
+++ static/viki_web_cms/js/orderElement/createOrderElement.js
@@ -3,7 +3,7 @@
 import {setupHeaderHandlers} from "../fullScreenElement/headerHandlers.js";
 import {createOrderRow} from "./createOrderRows.js";
 import {createPageContent} from "../fullScreenElement/createPageContent.js";
-import {getOrderData} from "./getOrderData";
+import {getOrderData} from "./getOrderData.js";
 
 /**
  * Creates user element with specified grid layout
@@ -13,8 +13,20 @@ import {getOrderData} from "./getOrderData";
 export async function createOrderElement(className) {
     const headerStyle = 'order-element__header';
     setupHeaderHandlers(getOrderData, createOrderRow, headerStyle);
-    const columns = [];
-    return await createPageContent('order-content',columns, headerStyle,
+    const columns = [
+        {title: 'номер'},
+        {title: 'дата'},
+        {title: 'от'},
+        {title: 'клиент'},
+        {title: 'статус'},
+        {title: 'дней'},
+        {title: 'срок'},
+        {title: 'макет'},
+        {title: 'счет'},
+        {title: 'накл.'},
+        {title: ''},
+    ];
+    return await createPageContent('order-content', columns, headerStyle,
         getOrderData, createOrderRow);
 }
 
