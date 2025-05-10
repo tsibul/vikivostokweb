c07ace4c5c95c950669a1ec44f8ce3d1acd36a9a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-10 19:06:22 +0300

    change for universal modules

--- static/viki_web_cms/js/orderElement/createOrderElement.js
+++ static/viki_web_cms/js/orderElement/createOrderElement.js
@@ -1,9 +1,9 @@
 'use strict';
 
 import {setupHeaderHandlers} from "../fullScreenElement/headerHandlers.js";
-import {createOrderRows} from "./createOrderRows.js";
+import {createOrderRow} from "./createOrderRows.js";
 import {createPageContent} from "../fullScreenElement/createPageContent.js";
-import {createPageHeader} from "../fullScreenElement/createPageHeader.js";
+import {getOrderData} from "./getOrderData";
 
 /**
  * Creates user element with specified grid layout
@@ -11,9 +11,10 @@ import {createPageHeader} from "../fullScreenElement/createPageHeader.js";
  * @returns {Promise<HTMLElement>} Created user element
  */
 export async function createOrderElement(className) {
-    setupHeaderHandlers(createOrderRows);
-    const columns = [];
     const headerStyle = 'order-element__header';
-    return await createPageContent('order-content', createPageHeader, createOrderRows, columns, headerStyle);
+    setupHeaderHandlers(getOrderData, createOrderRow, headerStyle);
+    const columns = [];
+    return await createPageContent('order-content',columns, headerStyle,
+        getOrderData, createOrderRow);
 }
 
