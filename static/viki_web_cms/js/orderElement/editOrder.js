14a1e8e78e088393648ec8942683d21b2b690b8d
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-11 01:57:20 +0300

    initial commit

--- static/viki_web_cms/js/orderElement/editOrder.js
+++ static/viki_web_cms/js/orderElement/editOrder.js
@@ -3,3 +3,15 @@
 export async function editOrder(e, row) {
 
 }
+
+export async function editItem(e, row) {
+
+}
+
+export async function editBranding(e, row) {
+
+}
+
+export async function editDelivery(e, row) {
+
+}
