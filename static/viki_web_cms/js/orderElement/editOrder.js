dee30ba2880e85bc0e57b78e657eeafea93755e3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-11 21:51:39 +0300

    bugfix tipe => type add edit item & branding ready

--- static/viki_web_cms/js/orderElement/editOrder.js
+++ static/viki_web_cms/js/orderElement/editOrder.js
@@ -6,6 +6,7 @@ import {createButtonBlock} from "../dictionaryElement/createInput/createButtonBl
 import {modalDnD} from "../modalFunction/modalDnD.js";
 import {fetchJsonData} from "../fetchJsonData.js";
 import {createOrderDropdown} from "./createOrderDropdown.js";
+import {createModalInput} from "../dictionaryElement/createInput/createModalInput.js";
 
 export const orderEditOptions = {
     'editOrder': {
@@ -13,7 +14,7 @@ export const orderEditOptions = {
         'content': editOrderContent,
     },
     'editItem': {
-        'tipe': 'editItem',
+        'type': 'editItem',
         'content': editItemContent,
     },
     'editBranding': {
@@ -137,7 +138,7 @@ async function sendOrderForm(e, fieldObj, form, initialData) {
  * @param {string} elementId
  * */
 function editOrderContent(data, modalContent, elementId) {
-
+    return modalContent;
 }
 
 /**
@@ -147,7 +148,32 @@ function editOrderContent(data, modalContent, elementId) {
  * @param {string} elementId
  * */
 function editItemContent(data, modalContent, elementId) {
-
+    const label = document.createElement('label');
+    label.htmlFor = `itemPrice_${elementId}`
+    label.textContent = 'цена';
+    label.classList.add('modal__content_label')
+    modalContent.appendChild(label);
+    const priceInput = createModalInput('number');
+    priceInput.id = `itemPrice_${elementId}`;
+    priceInput.name = 'price';
+    priceInput.step = '0.01';
+    priceInput.value = data.price;
+    priceInput.classList.add('modal__content_number');
+    modalContent.appendChild(priceInput);
+    if (Object.keys(data).includes('branding_name')) {
+        const brandingLabel = document.createElement('label');
+        brandingLabel.htmlFor = `itemBranding_${elementId}`
+        brandingLabel.textContent = 'нанесение';
+        brandingLabel.classList.add('modal__content_label')
+        modalContent.appendChild(brandingLabel);
+        const brandingInput = createModalInput('text');
+        brandingInput.id = `itemBranding_${elementId}`;
+        brandingInput.name = 'branding_name';
+        brandingInput.value = data.branding_name;
+        brandingInput.classList.add('modal__content_text');
+        modalContent.appendChild(brandingInput);
+    }
+    return modalContent;
 }
 
 /**
@@ -157,7 +183,41 @@ function editItemContent(data, modalContent, elementId) {
  * @param {string} elementId
  * */
 function editBrandingContent(data, modalContent, elementId) {
-
+    const printTypeLabel = document.createElement('div');
+    printTypeLabel.textContent = 'тип нанесения';
+    printTypeLabel.classList.add('modal__content_label');
+    printTypeLabel.htmlFor = `printType_${elementId}`;
+    modalContent.appendChild(printTypeLabel);
+    const printTypeDropDown = createOrderDropdown(data.available_print_types, data.print_type_id, 'print_type__id')
+    printTypeDropDown.id = `printType_${elementId}`
+    modalContent.appendChild(printTypeDropDown);
+    const printPlaceLabel = document.createElement('div');
+    printPlaceLabel.textContent = 'тип нанесения';
+    printPlaceLabel.classList.add('modal__content_label');
+    printPlaceLabel.htmlFor = `printPlace_${elementId}`;
+    modalContent.appendChild(printPlaceLabel);
+    const printPlaceDropDown = createOrderDropdown(data.available_print_places, data.print_place_id, 'print_place__id')
+    printPlaceDropDown.id = `printPlace_${elementId}`;
+    modalContent.appendChild(printPlaceDropDown);
+    const colorLabel = document.createElement('div');
+    colorLabel.textContent = 'кол-во цветов';
+    colorLabel.classList.add('modal__content_label');
+    colorLabel.htmlFor = `color_${elementId}`;
+    modalContent.appendChild(colorLabel);
+    const colorDropDown = createOrderDropdown(data.available_colors, data.colors, 'print_place__id');
+    colorDropDown.id = `color_${elementId}`;
+    modalContent.appendChild(colorDropDown);
+    const passLabel = document.createElement('div');
+    passLabel.textContent = 'второй проход';
+    passLabel.classList.add('modal__content_label');
+    passLabel.htmlFor = `pass_${elementId}`;
+    modalContent.appendChild(passLabel);
+    const pass = createModalInput('checkbox');
+    pass.name = 'second_pass'
+    pass.classList.add('check')
+    pass.id = `pass_${elementId}`;
+    modalContent.appendChild(pass);
+    return modalContent;
 }
 
 /**
@@ -169,6 +229,7 @@ function editBrandingContent(data, modalContent, elementId) {
 function editDeliveryContent(data, modalContent, elementId) {
     const label = document.createElement('div');
     label.textContent = 'доставка';
+    label.classList.add('modal__content_label')
     modalContent.appendChild(label);
     const dropDown = createOrderDropdown(data.available_options, elementId, 'id')
     modalContent.appendChild(dropDown);
