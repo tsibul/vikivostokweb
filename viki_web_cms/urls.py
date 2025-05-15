c351d9155f0e783e6972e60a2ad0c19c4e61a184
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-05-15 19:32:12 +0300

    show order file ready without view

--- viki_web_cms/urls.py
+++ viki_web_cms/urls.py
@@ -5,7 +5,6 @@ app_name = 'viki_web_cms'
 urlpatterns = [
     path('', views.main_cms, name='main'),
     path('customer', views.main_customer, name='main_customer'),
-
     path('json/field_names/<str:class_name>', views.field_names),
     path('json/field_values/<str:class_name>/<int:deleted>/<int:new_item>/<int:first_record>/<str:search_string>', views.field_values),
     path('json/record_info/<str:class_name>/<int:record_id>', views.record_info),
@@ -13,7 +12,6 @@ urlpatterns = [
     path('json/edit_dictionary/<str:class_name>/<int:element_id>', views.edit_dictionary),
     path('json/catalogue_data/<int:deleted>/<int:new_item>/<int:first_record>/<str:search_string>/<str:order>', views.catalogue_data),
     path('json/parse_file_data/<int:goods_id>/<int:simple_article>/<str:file_name>/<int:item_id>', views.parse_file_data),
-
     path('json/catalogue_record/<int:record_id>', views.catalogue_record),
     path('json/save_catalogue_item/<int:record_id>', views.save_catalogue_item),
     path('json/catalogue_csv_load', views.catalogue_csv_load),
