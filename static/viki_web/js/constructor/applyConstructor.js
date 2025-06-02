2c5670be253e220d491f948aff8e41066f21a5bd
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-06-02 11:39:10 +0300

    change simpleConstructor ready

--- static/viki_web/js/constructor/applyConstructor.js
+++ static/viki_web/js/constructor/applyConstructor.js
@@ -5,56 +5,23 @@
 
 'use strict'
 
+import {changeFrameItem} from "../product/changeFrameItem.js";
+import {closeConstructor} from "./closeConstructor.js";
+
+
 /**
  * Applies selected constructor options to the product
  * @param {HTMLElement} modal - Constructor dialog element
- * @param {HTMLElement} goodsItem - Product container element
+ * @param {number} itemId - Product container element
  * @param {HTMLElement} currentFrame - Current product frame element
  */
-export function applyConstructor(modal, goodsItem, currentFrame) {
-    const form = modal.querySelector('form');
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries());
-    const detailsNumber = parseInt(goodsItem.getAttribute('data-article-length'));
-    const articleLength = Object.keys(data).includes('option')
-        ? Object.keys(data).length - 1 : Object.keys(data).length;
-    
-    if (articleLength === detailsNumber) {
-        const goodsArticle = goodsItem.getAttribute('data-goods-article');
-        let article = goodsArticle;
-        
-        for (let i = 1; i <= articleLength; i++) {
-            article = article + '.' + data[i];
-        }
-        if (data['option']) {
-            article = article + '.' + data['option'];
-        }
-        
-        // Find the currently visible image frame
-        const visibleImageFrame = goodsItem.querySelector('.product-hor__image-frame:not(.item-hidden)') || 
-                                 goodsItem.querySelector('.product-sqr__image-frame:not(.item-hidden)');
-        
-        // Find any image frame that has the matching article
-        const newItem = goodsItem.querySelector(`.product-hor__image-frame[data-article="${article}"]`) || 
-                       goodsItem.querySelector(`.product-sqr__image-frame[data-article="${article}"]`);
-        
-        if (newItem) {
-            // Hide the currently visible frame
-            if (visibleImageFrame) {
-                visibleImageFrame.classList.add('item-hidden', 'item-opaque');
-            }
-            
-            // Show the new item
-            newItem.classList.remove('item-hidden', 'item-opaque');
-            modal.remove();
-        } else {
-            // Get price from the visible frame
-            const price = visibleImageFrame ? visibleImageFrame.dataset.price : '';
-            modal.querySelector('.simple-constructor__alert')
-                .textContent = 'Для заказа этого товара обратитесь к менеджеру. Цена: ' + price;
-        }
-    } else {
-        modal.querySelector('.simple-constructor__alert')
-            .textContent = 'Отметьте все элементы';
-    }
+export function applyConstructor(modal, itemId, currentFrame) {
+    const imageItems = currentFrame
+        .querySelectorAll(`div.product-hor__image-frame`);
+    const currentItem = [...imageItems].find(elem => !elem.classList.contains('item-hidden'));
+
+    const colorLabel = currentFrame.closest('.product')
+        .querySelector(`label.color-label[data-id="${itemId}"]`);
+    changeFrameItem(colorLabel, currentItem, itemId.toString())
+    closeConstructor(modal);
 }
