5d3977d52de336f13ea3d068daab0bde3f23ae1a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2025-06-08 20:17:12 +0300

    add group_management initial commit

--- static/viki_web_cms/js/groupElement/createGroupElement.js
+++ static/viki_web_cms/js/groupElement/createGroupElement.js
@@ -1,6 +1,31 @@
 'use strict';
 
-export async function createGroupElement (className) {
-    const groupContent = document.createElement('div')
-    return groupContent;
+import {setupHeaderHandlers} from "../fullScreenElement/headerHandlers.js";
+import {createPageContent} from "../fullScreenElement/createPageContent.js";
+import {createGroupRow} from "./createGroupRows.js";
+
+/**
+ * Creates user element with specified grid layout
+ * @param {string} className - CSS class name for the element
+ * @returns {Promise<HTMLElement>} Created user element
+ */
+export async function createGroupElement(className) {
+    const headerStyle = 'group-element__header';
+    setupHeaderHandlers(createGroupRow, headerStyle, '/cms/json/unused_files');
+    const columns = [
+        {title: 'название'},
+        {title: 'почта'},
+        {title: 'новый'},
+        {title: 'тип цены'},
+        {title: 'менеджер'},
+        {title: ''},
+        {title: ''},
+    ];
+    document.querySelector('.dictionary-frame__header_right').innerHTML = '';
+    return await createPageContent('group-content', columns, headerStyle,
+        createGroupRow, '/cms/json/unused_files');
 }
+
+
+
+
